ceed13cf50671a6c987c539a4aa9b6ad7603a5a0
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-07-26 22:35:10 -0400

    makes small change' '

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   return (
 
-  <div>
+  <>
 
     
   
@@ -15,7 +15,9 @@ function App() {
     <Primary />
     <Footer />
     
-  </div>
+  </>
+    
+ 
  
   
     
