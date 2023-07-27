810c6a82cf6b884d284b8aa37eb06b7ef7485273
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-07-26 21:53:49 -0400

    recreates the css grid lab

---
+++ src/components/primary.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Primary = () => {
+    return (
+        <div class="primary-content">
+            <div class="sidebar">
+                <p>Sidebar.</p>
+            </div>
+
+            <div class="main">
+                <p>Main content goes here.</p>
+            </div>
+        </div>
+
+    )
+}
+export default Primary;
